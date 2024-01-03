1fa1c58ff5500082f616be75f2e119479115687b
KrasovskyPavel <frostcat2002@gmail.com>
2024-01-03 15:25:56 +0300

    Add: all necessary server files

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const { login, register, current } = require("../controllers/users");
+const { auth } = require("../middleware/auth");
 
 /* login */
 router.post("/login", login);
 /* register */
 router.post("/register", register);
 /* current */
-router.get("/current", current);
+router.get("/current", auth, current);
 
 module.exports = router;
