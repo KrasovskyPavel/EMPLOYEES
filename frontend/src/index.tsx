4951a9ec8a8e2d7c6b5320046d06d67f65d8ce45
KrasovskyPavel <frostcat2002@gmail.com>
2024-01-04 08:31:09 +0300

    Add: frontend basic structure

---
+++ frontend/src/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Paths } from "./paths";
+
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h1>Home</h1>,
+  },
+  {
+    path: Paths.login,
+    element: <h1>Login</h1>,
+  },
+  {
+    path: Paths.register,
+    element: <h1>Register</h1>,
+  },
+]);
+
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
